ee4052d1a627e53615c0d237450170af9c0c126a
iamgift <giftpeteredu@gmail.com>
2023-08-30 14:31:33 -0700

    First Website Commit

---
+++ Javascript/index.js
@@ -0,0 +1,10 @@
+let menu = document.querySelector('.menu-icon');
+let mobileMenu = document.querySelector('.mobile-menu');
+
+function hideMenu(){
+    mobileMenu.style.display = "block";
+}
+
+function handleClose(){
+    mobileMenu.style.display = "none";
+}
